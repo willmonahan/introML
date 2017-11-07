9280bde664c9436883a791b5d211810a2da36dc2
willmonahan <willmonahan13@gmail.com>
2017-11-07 18:17:46 -0500

    use a decision tree to predict an apple or an orange!

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,13 @@
 #hello world!
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]] #1 is smooth, 0 is bumpy
+labels = [0, 0, 1, 1] #0 is apple, 1 is orange
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+newFeatures = [[160,0],[135,1]]
+newFruit = clf.predict(newFeatures)
+
+print(newFruit)
