c1a2c1b7fe4b34751b6f89d9eb4f6a7c3d107a49
willmonahan <willmonahan13@gmail.com>
2017-11-07 19:13:25 -0500

    super crappy random classifier. has ~33% accuracy

--- lesson5.py
+++ lesson5.py
@@ -1,3 +1,18 @@
+import random
+
+class ScrappyKNN():
+	def fit(self, X_train, y_train):
+		self.X_train = X_train
+		self.y_train = y_train
+
+	def predict(self, X_test):
+		predictions = []
+		for row in X_test:
+			label = random.choice(self.y_train)
+			predictions.append(label)
+
+		return predictions
+
 #import datasets
 from sklearn import datasets
 iris = datasets.load_iris()
@@ -8,8 +23,9 @@ y = iris.target
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
-from sklearn.neighbors import KNeighborsClassifier
-my_classifier = KNeighborsClassifier()
+#from sklearn.neighbors import KNeighborsClassifier
+my_classifier = ScrappyKNN()
+
 my_classifier.fit(X_train, y_train)
 
 predictions = my_classifier.predict(X_test)
