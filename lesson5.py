d57ead489f4e94a9be2915da70ed64687c384f19
willmonahan <willmonahan13@gmail.com>
2017-11-07 19:34:54 -0500

    I wrote a K-Nearest-Neighbors classifier!!!!!

--- lesson5.py
+++ lesson5.py
@@ -1,4 +1,7 @@
-import random
+from scipy.spatial import distance
+
+def euc(a,b):
+	return distance.euclidean(a,b)
 
 class ScrappyKNN():
 	def fit(self, X_train, y_train):
@@ -8,10 +11,19 @@ class ScrappyKNN():
 	def predict(self, X_test):
 		predictions = []
 		for row in X_test:
-			label = random.choice(self.y_train)
+			label = self.closest(row)
 			predictions.append(label)
 
 		return predictions
+	def closest(self, row):
+		best_dist = euc(row, self.X_train[0])
+		best_index = 0
+		for i in range(1, len(self.X_train)):
+			dist = euc(row, self.X_train[i])
+			if dist < best_dist:
+				best_dist = dist
+				best_index = i
+		return self.y_train[best_index]
 
 #import datasets
 from sklearn import datasets
